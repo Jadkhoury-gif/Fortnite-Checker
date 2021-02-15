2dbd492f6f3173086c67e6578f69d1930035ffe5
Jadkhoury-gif <79093972+Jadkhoury-gif@users.noreply.github.com>
2021-02-15 17:55:13 +0200

    Update Proxy.cs

--- src/FortniteChecker/Models/Proxy.cs
+++ src/FortniteChecker/Models/Proxy.cs
@@ -1,10 +1,18 @@
-﻿namespace FortniteChecker.Models
-{
-    internal class Proxy
-    {
-        public string IpAddress { get; set; }
-        public ushort Port { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
+
+LOGIN:j.h.hudson1988@hotmail.com:leonjay1988 
+LOGIN:matteovrenna1@gmail.com:matteino2003 
+LOGIN:xocierrarene@gmail.com:Cierra09 
+LOGIN:kandice86@gmail.com:Kaylee2006 
+LOGIN:prestonshazri@gmail.com:Boomershaz13 
+LOGIN:greeeken_1991@hotmail.com:Rz650401 
+LOGIN:timmy22@libero.it:Storia22 
+LOGIN:kuepfer14@gmail.com:cool1997 
+LOGIN:rstuursma1@gmail.com:Iy4izw1k 
+LOGIN:alecutolo04@gmail.com:V150884p 
+LOGIN:skaarsgard1@gmail.com:Shadowcallie1 
+LOGIN:ch38474@gmail.com:Electricoke269 
+LOGIN:fontaineclem23@gmail.com:04N741clem. 
+LOGIN:gocubsgo44@gmail.com:Sammysosa13 
+LOGIN:grecoaustin117@gmail.com:deerwood1545 
+LOGIN:mjerrickp@gmail.com:Mjp26943 
+LOGIN:ribarr15@yahoo.com:Ibarra299
